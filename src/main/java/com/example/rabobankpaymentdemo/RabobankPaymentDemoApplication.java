173b96190110a39ed6441f0494a491585ea9cbb5
Masum Islam <madsum@gmail.com>
2022-01-26 08:24:34 +0100

    Add spring boot generated project

---
+++ src/main/java/com/example/rabobankpaymentdemo/RabobankPaymentDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabobankpaymentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabobankPaymentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabobankPaymentDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rabobankpaymentdemo/RabobankPaymentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabobankpaymentdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabobankPaymentDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
